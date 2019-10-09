43c12bd855a75a8462d97fbc8d04119148179fa8
Valéria Nicéria <valerianiceria@gmail.com>
2019-10-08 22:16:13 -0300

    update

--- ui/header.R
+++ ui/header.R
@@ -1,7 +1,7 @@
 header = shinydashboardPlus::dashboardHeaderPlus(
   title = tagList(
-    span(class = "logo-lg", ""), 
-    img(src = "img/arvore.svg")),
+    span(class = "logo-lg", "Queimadas"), 
+    img(class="logo-mini", style="width: 42px; margin-left: -10px;", src = "img/arvore.svg")),
   enable_rightsidebar = TRUE,
   rightSidebarIcon = "gears"
 )
