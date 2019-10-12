a6ba834f5d857c1ec5cf669a6835aafb79b68605
Valéria Nicéria <valerianiceria@gmail.com>
2019-10-12 16:48:29 -0300

    sidebar

--- ui/sidebar.R
+++ ui/sidebar.R
@@ -1,1 +1,9 @@
-sidebar = shinydashboard::dashboardSidebar()
+sidebar = shinydashboard::dashboardSidebar(
+  shinydashboard::sidebarMenu(
+    shinydashboard::menuItem(
+      text = "Home",
+      icon = icon("home"),
+      tabName = "tabHome"
+    )
+  )
+)
